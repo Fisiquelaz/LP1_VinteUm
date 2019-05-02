4905489224f445f54ee6577a0b157f9a02607b97
João Mendes Lopes Neto <joaomendes_ln@A204-10L.imd.local>
2019-05-02 12:07:12 -0300

    Correção para o caso de empate e de todos perderem

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ int main(){
 		cin >> nome;
 		jogadores[i].set_nome(nome);
 	}
-	int vencer = 0, count_rodada = 1, count_nao_ativo = 0, count_perdeu = 0, opcao, cond_parada, result_dado, vencedor_rodada = -1;
+	int vencer = 0, count_rodada = 1, count_nao_ativo = 0, count_perdeu = 0, opcao = 2, cond_parada, result_dado1, result_dado2, vencedor_rodada = -1, empate = 0;
 	// Dado dado;
 	srand (time(NULL));
 
@@ -51,11 +51,13 @@ int main(){
 		cout << "\nJOGADA " << count_rodada << endl;
 		for(int i = 0; i < vinte_um.get_qtd_jogadores(); i++){
 			if(jogadores[i].get_status() == 0){ // o jogador só lança os dados se seu status ainda for ativo
-				cout << jogadores[i].get_nome() << endl;
-				result_dado = rand() % 6 + 1;
-				jogadores[i].acumula_pontuacao(result_dado);
+				cout << "Jogador: " << jogadores[i].get_nome() << endl;
+				result_dado1 = rand() % 6 + 1;
+				result_dado2 = rand() % 6 + 1;
+				jogadores[i].acumula_pontuacao(result_dado1);
+				jogadores[i].acumula_pontuacao(result_dado2);
 
-				cout << "O resultado do seu lançamento foi: " << result_dado << endl;
+				cout << "Os resultados do seu lançamento foram: " << result_dado1 << " e " << result_dado2 << endl;
 
 				if(jogadores[i].get_pontuacao() > vinte_um.get_pontuacao_maxima()){ // se a pontuação acumulada do jogador for maior que a pontuação máxima, ele perde
 					jogadores[i].altera_status(2);
@@ -69,11 +71,17 @@ int main(){
 						break;
 					}
 					else{
-						cout << "Deseja continuar? 0 - Não; 1 - Sim" << endl;
-						cin >> opcao;
+						while (opcao != 0 && opcao != 1){
+							cout << "Deseja continuar? 0 - Não; 1 - Sim" << endl;
+							cin >> opcao;
+							if (opcao != 0 && opcao != 1){
+								cout << "Digite 0 ou 1" << endl;
+							} 
+						}
 						if(opcao == 0){ // se o jogador decidir não continuar, o seu status é alterado para inativo
 							jogadores[i].altera_status(1);
 						}
+						opcao = 2;
 					}
 				}
 			}
@@ -83,15 +91,9 @@ int main(){
 			if (jogadores[i].get_status() == 2) cout << jogadores[i].get_nome() << ": Perdeu" << endl;
 			if (jogadores[i].get_status() != 2)	{
 				cout << jogadores[i].get_nome() << ": " << jogadores[i].get_pontuacao() << endl;
-				if (vencedor_rodada == -1) vencedor_rodada = i;
-				else {
-					if (jogadores[i].get_pontuacao() > jogadores[vencedor_rodada].get_pontuacao()) vencedor_rodada = i;
-				}
 			}
 		}
-		cout << "Vencedor da rodada: " << jogadores[vencedor_rodada].get_nome() << endl;
 		count_rodada += 1;
-		vencedor_rodada = -1;
 
 		if(vencer == 0){
 			for(int i = 0; i < vinte_um.get_qtd_jogadores(); i++){
@@ -106,9 +108,13 @@ int main(){
 				vencer = 1;
 				cond_parada = 1;
 			}
-			if (vinte_um.get_qtd_jogadores() == count_nao_ativo){ // condição de parada para o caso de todos os jogadores não estarem mais ativos
+			if (vinte_um.get_qtd_jogadores() == count_nao_ativo && vinte_um.get_qtd_jogadores() != count_perdeu){ // condição de parada para o caso de todos os jogadores não estarem mais ativos
 				vencer = 1;
 				cond_parada = 2;
+			}
+			if (vinte_um.get_qtd_jogadores() == count_perdeu){ // condição de parada para o caso de todos os jogadores já terem perdido
+				vencer = 1;
+				cond_parada = 4;
 			}
 		}
 		count_nao_ativo = 0;
@@ -119,7 +125,7 @@ int main(){
 
 	if (cond_parada == 1){
 		for(int i = 0; i < vinte_um.get_qtd_jogadores(); i++){
-			if (jogadores[i].get_status() != 2) cout << jogadores[i].get_nome() << " venceu!" << endl;
+			if (jogadores[i].get_status() != 2) cout << jogadores[i].get_nome() << " venceu a rodada!" << endl;
 		}
 	}
 	if (cond_parada == 2){
@@ -131,12 +137,25 @@ int main(){
 				}
 			}
 		}
-		cout << jogadores[vencedor_rodada].get_nome() << " venceu!" << endl;
+		for(int i = 0; i < vinte_um.get_qtd_jogadores(); i++){ // análise do caso se outro jogador atingiu a pontuação do ganhador, ou seja, houve empate
+			if(jogadores[i].get_status() != 2){
+				if (jogadores[i].get_pontuacao() == jogadores[vencedor_rodada].get_pontuacao() && i != vencedor_rodada) {
+					if (empate == 0) cout << "Os vencedores foram " << jogadores[vencedor_rodada].get_nome();
+					cout << ", " << jogadores[i].get_nome();
+					empate = 1;
+				}
+			}
+		}
+		if (empate == 0) cout << jogadores[vencedor_rodada].get_nome() << " venceu a rodada!";
+		cout << endl;
 	}
 	if (cond_parada == 3){
 		for(int i = 0; i < vinte_um.get_qtd_jogadores(); i++){
-			if (jogadores[i].get_status() == 3) cout << jogadores[i].get_nome() << " venceu!" << endl;
+			if (jogadores[i].get_status() == 3) cout << jogadores[i].get_nome() << " venceu a rodada!" << endl;
 		}
+	}
+	if (cond_parada == 4) {
+		cout << "Não houve vencedores na rodada" << endl;
 	}
 
 	return 0;
